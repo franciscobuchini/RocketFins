10febd65c96486ac05f2a8e0eb194bec6650fdd9
franciscobuchini <franciscobuchini@gmail.com>
2024-07-15 16:07:23 +0200

    background modification, style changes

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -5,7 +5,14 @@ function Home() {
   return (
     <>
       <article>
-
+        <p>Go to order</p>
+        <p>Blog section: New releases, next dates.</p>
+        <p>Instagram API</p>
+        <p>Other products</p>
+        <p>Contact information</p>
+        <p>FAQ</p>
+        <p>PDF info</p>
+        <p>Shipping Information</p>
       </article>
     </>
   );
