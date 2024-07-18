331d7dca2243eb4d0b142298cccdbb354aa5c9af
franciscobuchini <franciscobuchini@gmail.com>
2024-07-18 17:29:29 +0200

    starting with Home view

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -1,11 +1,16 @@
 // Home.jsx
+import img01 from '../Assets/img01.webp'
 
 function Home() {
 
   return (
     <>
+      <article id='goToOrder'>
+        <img src={img01}/>
+        <h1> Design your own fins </h1>
+        <button>Let's do it</button>
+      </article>
       <article>
-        <p>Go to order</p>
         <p>Blog section: New releases, next dates.</p>
         <p>Instagram API</p>
         <p>Other products</p>
