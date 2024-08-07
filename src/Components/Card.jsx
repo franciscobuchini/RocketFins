3551b7cd40bab51a76b3098fd916489a096ed5a1
franciscobuchini <franciscobuchini@gmail.com>
2024-08-07 18:43:46 +0200

    new design

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,13 +1,15 @@
 //Card.jsx
 
-function Card(img) {
+function Card({img}) {
 
   return (
     <div className="card">
       <img src={img}/>
-      <h4>Product name</h4>
-      <p>Description</p>
-      <p>Price</p>
+      <div>
+        <h4>Product name</h4>
+        <p>Description</p>
+        <p>Price</p>
+      </div>
     </div>
   );
 }
--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -1,6 +1,7 @@
 // Home.jsx
 import { Link } from 'react-router-dom';
 import Card from "./../Components/Card.jsx"
+import fins01 from "./../Assets/fins01.webp"
 import img01 from "./../Assets/img01.webp"
 
 function Home() {
@@ -9,7 +10,19 @@ function Home() {
     <section>
       <article id='Title'>
         <img src={img01}/>
-        <h1>THE NEXT-GEN FINS FOR UNDERWATER SPORTS</h1>
+        <h1>THE NEXT GEN FINS FOR UNDERWATER SPORTS</h1>
+      </article>
+
+      <article id="cardsRow">
+        <h2> The best selection: </h2>
+        <div>
+          <Card img={fins01}/>
+          <Card img={fins01}/>
+          <Card img={fins01}/>
+          <Card img={fins01}/>
+          <Card img={fins01}/>
+        </div>
+
       </article>
 
       <article id='goToOrder'>
@@ -18,6 +31,7 @@ function Home() {
           <button>Let's do it</button>
         </Link>
       </article>
+
       <article>
         <h2>Shop with Purpose: Support Ukraine</h2>
         <p>At Rocketfin, we believe in the power of sports to unite and uplift communities. That's why, with every pair of fins you purchase, a portion of the sale goes directly to humanitarian aid in Ukraine.</p>
@@ -27,12 +41,7 @@ function Home() {
           <button>Purchase Now and Support Ukraine</button>
         </Link>
       </article>
-      <article id="cardsRow">
-        <Card img={img01}/>
-        <Card img={img01}/>
-        <Card img={img01}/>
-        <Card img={img01}/>
-      </article>
+      
       <article>
         <p>* Shipping Information</p>
         <p>Language Switcher</p>
